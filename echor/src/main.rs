8913e50a4502481d7a0c9debbdcc1eb100250601
akthrms <akt.hrms@gmail.com>
2022-06-11 14:25:10 +0900

    update

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(
+    version = "0.1.0",
+    author = "akthrms <akt.hrms@gmail.com>",
+    about = "Rust echo"
+)]
+struct Config {
+    /// Input text
+    #[clap(value_name = "TEXT", required = true, min_values = 1)]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[clap(short = 'n', takes_value = false)]
+    omit_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let Config { text, omit_newline } = Config::parse();
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
